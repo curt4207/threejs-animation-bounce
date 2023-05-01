cbec254c80e87cc1a6ddac7bb7b64c5daa5920c7
curtis clayton <curtisclayton4270@gmail.com>
2023-05-01 12:12:08 -0400

    Added ground box and ball

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
-import MainContainer from "./MainContainer"
-import { Canvas } from "@react-three/fiber"
+import MainContainer from "./MainContainer";
+import { Canvas } from "@react-three/fiber";
+// Import three/rapier
+import { Physics, RigidBody } from "@react-three/rapier";
+import { Suspense } from "react";
 
 function App() {
- 
   return (
-<Canvas>
-   <MainContainer/>
-</Canvas>
-  )
+    <Canvas shadows camera={{ position: [10, 15, 35], fov: 40 }}>
+      <Suspense>
+        <Physics debug>
+          <MainContainer />
+        </Physics>
+      </Suspense>
+    </Canvas>
+  );
 }
 
-export default App
-
+export default App;
--- src/MainContainer.jsx
+++ src/MainContainer.jsx
@@ -1,54 +1,109 @@
+import { useRef } from "react";
+import { useState } from "react";
 
-import { OrbitControls,
-     Sphere,
-      // SpotLight,
-       useHelper
-       } from "@react-three/drei";
+import * as THREE from "three";
 
-  import { useRef } from "react";
-  import * as THREE from "three";
-  import { useTexture } from "@react-three/drei";
-  
-  
+// Import three/drei
+import {
+  OrbitControls,
+  Sphere,
+  // SpotLight,
+  useHelper,
+} from "@react-three/drei";
+
+import { Box } from "@react-three/drei";
+import { useTexture } from "@react-three/drei";
+import { useFrame } from "react-three-fiber";
+import { Shadow } from "@react-three/drei";
+
+
+// Import three/rapier
+import {  RigidBody, quat } from "@react-three/rapier";
+
 const MainContainer = () => {
-    const directionalLightRef = useRef();
-    const directionalLightRefTwo = useRef();
-    // const [earthTexture, planetSecondMap] = useTexture(["/8k_earth_dayMap.jpg","/8k_earth_dayMap.jpg"])
-   
+  const [hover, setHover] = useState(false);
+  const sphere = useRef();
+  const kicker = useRef();
+  const [start, setStart] = useState(false);
+
+  const jump = () => {
+    sphere.current.applyImpulse({ x: 0, y: 50, z: 0 });
+    // console.log("jump")
+  };
+
+  const directionalLightRef = useRef();
+  const directionalLightRefTwo = useRef();
+  // const [earthTexture, planetSecondMap] = useTexture(["/8k_earth_dayMap.jpg","/8k_earth_dayMap.jpg"])
 
   useHelper(directionalLightRef, THREE.DirectionalLightHelper, 1, "yellow");
   useHelper(directionalLightRefTwo, THREE.DirectionalLightHelper, 1, "green");
 
+  useFrame((_state, delta) => {
+    if(!start) return
+
+    const currentRotation = quat(kicker.current.rotation());
+    const incrementRotation = new THREE.Quaternion().setFromAxisAngle(
+      new THREE.Vector3(0, 1, 0),
+      delta * 1.5
+    );
+    currentRotation.multiply(incrementRotation);
+    kicker.current.setNextKinematicRotation(currentRotation);
+  });
+
   return (
     <>
-      <color attach="background" args={["black"]} />
+      <color attach="background" args={["#030103"]} />
       <OrbitControls />
-
-
       {/* NOTE: Lighting position x,y,z */}
       <directionalLight
         ref={directionalLightRef}
-        position={[0, 0, 10]}
-        intensity={1}
+        position={[0, 5, 15]}
+        intensity={1.5}
       />
-      <directionalLight
+      {/* <directionalLight
         ref={directionalLightRefTwo}
-        position={[0, 0, -10]}
-        intensity={0.5}
-      />
+        position={[0, 10, -20]}
+        intensity={0.3}
+      /> */}
 
-      {/* NOTE: Planet */}
-      <mesh
-        visible
-        userData={{ hello: "world" }} 
+      <RigidBody
+        ref={sphere}
+        position={[0, 5, 0]}
+        colliders={"ball"}
+        restitution={1.6}
+        gravityScale={4}
       >
-        <sphereGeometry args={[1, 34, 34]} />
-        <meshStandardMaterial  color="green"
-        // map={earthTexture} 
-        // normalMap={planetNormalMap}
-         />
-      </mesh>
-      
+        <Sphere
+          onPointerEnter={() => setHover(true)}
+          onPointerLeave={() => setHover(false)}
+          onClick={jump} 
+        >
+        
+          <meshStandardMaterial color={hover ? "#0DF205" : "hotpink"} />
+        </Sphere>
+      </RigidBody>
+
+      {/* KICKER */}
+      <RigidBody type="kinematicPosition" position={[0, 0.5, 0]} ref={kicker}>
+        <group position={[0, 0, 0]}>
+          <Box args={[20, 1, 1]}
+
+          onClick={(() => setStart(true))}
+          >
+            <meshStandardMaterial color="red" />
+          </Box>
+        </group>
+      </RigidBody>
+
+      {/* GROUND */}
+
+      <RigidBody type="fixed">
+        <Box position={[0, 0, 0]} args={[40, 0.1, 40]}
+        
+        >
+          <meshStandardMaterial color="#6379F2" />
+        </Box>
+      </RigidBody>
     </>
   );
 };
